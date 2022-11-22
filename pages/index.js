5b94975d4e478de3b7a82398cebd58361609a864
jedposter <114614617+jedposter@users.noreply.github.com>
2022-11-22 12:44:34 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="You've reached Jed Poster." />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Please leave a message after the beep <code>jed@jedposter.com</code>
         </p>
       </main>
 
